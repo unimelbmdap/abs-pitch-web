c42cbf702a10de01dc1c11a7127569e169f7fc09
Damien Mannion <damien@djmannion.net>
2024-06-22 15:05:01 +1000

    -

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,34 @@
 
 import {initAudio} from './audio.js'
 
+const BASE_FREQ = 196;
+
+const SCALE_MIN_CENTS = -48;
+const SCALE_MAX_CENTS = +2848;
+const SCALE_STEP_CENTS = 4;
+const SCALE_CENTS = [];
+for (let scaleValue=SCALE_MIN_CENTS; scaleValue <= SCALE_MAX_CENTS; scaleValue += SCALE_STEP_CENTS) {
+  SCALE_CENTS.push(scaleValue);
+}
+
+
+// notes and their offsets in cents from `BASEFREQ`
+const NOTES = {
+  "A♭": 100,
+  "A": 200,
+  "B♭": 300,
+  "B": 400,
+  "C": 500,
+  "C#": 600,
+  "D": 700,
+  "E♭": 800,
+  "E": 900,
+  "F": 1000,
+  "F#": 1100,
+  "G": 0,
+};
+
+
 
 async function main() {
 
@@ -16,6 +44,84 @@ async function main() {
 
 
 }
+
+
+function genStartCents(prevResponse) {
+
+  const minDeltaCents = 1460;
+
+  let candCents;
+
+  let ok = false;
+
+  while (!ok) {
+
+    ok = true;
+
+    candCents = randomChoice(SCALE_CENTS);
+
+    if (Math.abs(candCents - prevResponse) <= minDeltaCents) {
+      ok = false;
+    }
+    else if (mod(candCents, 100) === 0) {
+      ok = false;
+    }
+
+  }
+
+  return candCents;
+
+}
+
+
+function genBlockSequence(lastNote) {
+
+  const orderedNotes = Object.keys(NOTES);
+
+  const notes = Object.keys(NOTES);
+
+  let ok = false;
+
+  while (!ok) {
+
+    shuffle(notes);
+
+    ok = true;
+
+    let testArray = [lastNote].concat(notes);
+
+    for (let i=1; i < testArray.length; i++) {
+
+      let currNote = testArray[i];
+      let prevNote = testArray[i - 1];
+
+      let iCurrNote = orderedNotes.indexOf(currNote)
+      let iPrevNote = orderedNotes.indexOf(prevNote);
+
+      if (iCurrNote === 0) {
+        if (iPrevNote === (orderedNotes.length - 1) || iPrevNote === 1) {
+          ok = false;
+        }
+      }
+      else if (iCurrNote == (orderedNotes.length - 1)) {
+        if (iPrevNote === (iCurrNote - 1) || iPrevNote === 0) {
+          ok = false;
+        }
+      }
+      else {
+        if (iPrevNote == (iCurrNote - 1) || iPrevNote == (iCurrNote + 1)) {
+          ok = false;
+        }
+      }
+
+    }
+
+  }
+
+  return notes;
+
+}
+
 
 async function runStartPhase() {
   showOverlay('startOverlayContainer');
@@ -90,7 +196,9 @@ async function runTrial(trialConfig, audio) {
 
   const slider = document.getElementById('pitchSlider');
 
-  slider.value = 1000;
+  const randStartCents = genStartCents(trialConfig.prevResponse);
+
+  slider.value = randStartCents;
 
   slider.addEventListener('input', handleSlider);
   slider.dispatchEvent(new CustomEvent('input'));
@@ -182,6 +290,28 @@ async function waitForPeriod(timeS) {
 
 function centsToFreq(cents, baseFreq) {
   return baseFreq * (2 ** (cents / 1200));
+}
+
+function shuffle(array) {
+  // from https://javascript.info/task/shuffle
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+function randomChoice(seq) {
+  return seq[randomInteger(0, seq.length - 1)];
+}
+
+function randomInteger(min, max) {
+  // from https://javascript.info/number
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
 }
 
 window.addEventListener("load", main);
